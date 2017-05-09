b4e1af32b8b3002ff5e519aa3b2d5e68ac665422
Richard Wilson <wilsonras@outlook.com>
2017-05-09 16:54:58 +0100

    Add project files.

---
+++ PricingTool/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PricingTool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1071, 343);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 349);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1071, 238);
+            this.listBox1.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 901);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMain";
+            this.Text = "Pricing Tool ...";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PricingTool/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RASW;
+using System.Data.OleDb;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace PricingTool
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string fileName = "Polarcirkel Parts List for new template.xlsx";
+            DataTable data = ReadSpreadsheet(Path.Combine(Directory.GetCurrentDirectory() + @"\Spreadsheets", fileName), null);
+            dataGridView1.DataSource = data;
+
+            List<string> list = ProcessDataToList(data);
+
+            DateTime dt = new DateTime();
+            double xChNOKtoGB = 0;
+            CreateInsertSQL(list, DateTime.Now,xChNOKtoGB);
+        }
+
+        private void CreateInsertSQL(List<string> list, DateTime date, double ExchangeRateNOKtoGB)
+        {
+            string tempInsertSQL = Path.Combine(Directory.GetCurrentDirectory(), "insert.sql");
+
+            string sql = "SET DATEFORMAT dmy; INSERT INTO[dbo].[PolarcirkleCostPrices] ([polarPriceListDate],[polarExchangeRateUsedID],[polarPartNumber],[polarAkvaPartNumber],[polarAkvaPartNames],[polarMerdedler],[polarPartDescription],[polarUnit],[polarAGcostNOK],[polarAGcostOnRequest],[polarCostGBP],[polarDateTimeAdded],[polarValid]) VALUES (";
+
+            string sqlEnd = "')"; // SELECT SCOPE_IDENTITY();";
+
+            if (File.Exists(tempInsertSQL)) File.Delete(tempInsertSQL);
+
+            try
+            {
+                foreach (string line in list)
+                {
+                    if (line != "|||||||")
+                    {
+                        string[] split = line.Split('|');
+
+                        if (split[0].Substring(0, 5).ToLower() != "polar")
+                        {
+                            string sqlData = "'" + date.ToShortDateString() + "','" + ExchangeRateNOKtoGB + "','" + split[0].ToString() + "','" + split[1].ToString() + "','" + split[2].ToString() + "','" + split[3].ToString() + "','" + split[4].ToString() + "','" + split[5].ToString() + "','" + split[6].ToString() + "','" + split[7].ToString() + sqlEnd;
+                            File.AppendAllText(tempInsertSQL, sql + sqlData + Environment.NewLine);
+                        }
+                       
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+            }
+
+            //try
+            //{
+            //    using (SqlConnection cnn = new SqlConnection(ConnectionString))
+            //    {
+            //        cnn.Open();
+            //        using (SqlCommand cmd = new SqlCommand(scriptLine.ToString(), cnn))
+            //        {
+            //           int lastID = (int)cmd.ExecuteScalar();
+            //           cmd.Dispose();
+            //           cnn.Close();
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    //lstOutput.Items.Add("");
+            //    //lstOutput.Items.Add("ERROR: " + ex.Message);
+            //    //lstOutput.Items.Add("");
+            //    //return "";
+            //}
+        }
+
+        private List<string> ProcessDataToList(DataTable data)
+        {
+            List<string> sheetRows = new List<string>();
+                      
+            try
+            {
+                foreach (DataColumn col in data.Columns)
+                {
+                    foreach (DataRow row in data.Rows)
+                    {
+                        //listBox1.Items.Add(row[0] + "|" + row[1] + "|" + row[2] + "|" + row[3] + "|" + row[4] + "|" + row[5] + "|" + row[6] + "|" + row[7]);
+                        sheetRows.Add(row[0] + "|" + row[1] + "|" + row[2] + "|" + row[3] + "|" + row[4] + "|" + row[5] + "|" + row[6] + "|" + row[7]);
+                    }
+                }
+
+                return sheetRows;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        DataTable ReadSpreadsheet(string SpreadsheetFullPath, string SheetName)
+        {
+            //Polarcirkel Parts List for new template.xlsx
+
+            if (SheetName == null) { SheetName = "Sheet1"; }
+
+            DataTable dt = new DataTable();
+            try
+            {
+                using (OleDbConnection conn = new OleDbConnection())
+                {
+                    conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + SpreadsheetFullPath + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;MAXSCANROWS=0'";
+                    using (OleDbCommand comm = new OleDbCommand())
+                    {
+                        comm.CommandText = "Select * from [" + SheetName + "$]";
+                        comm.Connection = conn;
+
+                        using (OleDbDataAdapter da = new OleDbDataAdapter())
+                        {
+                            da.SelectCommand = comm;
+                            da.Fill(dt);
+                        }
+                    }
+                    conn.Close();
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+    }
+}
